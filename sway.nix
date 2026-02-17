c879ae896c0cfdfa3f9689d341a8c1bf6cb50204
pau-miralles <pmms0808@gmail.com>
2026-02-17 22:37:15 +0100

    Cleanup

--- sway.nix
+++ sway.nix
@@ -1,26 +1,21 @@
 { config, pkgs, ... }:
-
 {
-  # Enable OpenGL (required for Wayland)
-  # Note: On older NixOS versions this was called hardware.opengl
+  # OpenGL (required for Wayland)
   hardware.graphics.enable = true;
 
-  # Enable the login manager (SDDM)
   services.displayManager.sddm.enable = true;
   services.displayManager.sddm.wayland.enable = true;
 
-  # Enable Sway
   programs.sway = {
     enable = true;
     package = pkgs.swayfx; # Use SwayFX instead of standard Sway
     wrapperFeatures.gtk = true; # Fixes GTK apps in Sway
   };
 
-  # Essential packages for the GUI
   environment.systemPackages = with pkgs; [
     waybar       # Status bar
-    wofi         # App launcher (like Start menu)
-    kitty        # Terminal Emulator
+    wofi         # App launcher
+    kitty        # Terminal
     mako         # Notification daemon
     libnotify    # To send notifications
     swaybg       # Wallpapers in Sway
@@ -31,18 +26,15 @@
     polkit_gnome # Authentication agent
   ];
 
-  # Install the Ubuntu Mono Nerd Font
   fonts.packages = with pkgs; [
     nerd-fonts.ubuntu-mono
   ];
 
-  # XDG Portals (Crucial for file dialogs and screen sharing)
   xdg.portal = {
     enable = true;
     wlr.enable = true;
     extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
   };
-  
-  # Security (needed for some GUI apps to ask for passwords)
+
   security.polkit.enable = true;
 }
